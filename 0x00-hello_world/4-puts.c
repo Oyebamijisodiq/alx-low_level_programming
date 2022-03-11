aafe4cc04b5322c188357137efa263064760b0d9
oyebamiji sodiq <oyebamijisodiq70@gmail.com>
2022-03-11 01:51:27 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
-#include <stdio.h>
-/**
-[3~[3~[D[D[D[D[D[D[D * main - main block
- *Return: 0
- */[A[D Return: 0[B[D[D[D[D[D[D[D[D[D
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+	#include <stdio.h>
+
+	/**
+	 * main - Entry point
+	 *
+	 * Description: prints "Programming is like building a multilingual puzzle"
+	*
+	* Return: Always 0 (Success)
+	*/
+[D[A[A[A	 * * RetuAlways 0 (Success)[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D */
+	int main(void)
+	{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+	}
