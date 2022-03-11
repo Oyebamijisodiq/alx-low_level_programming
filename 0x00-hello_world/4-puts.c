838705eec266c0870bb7a945d601fecd42cebdee
oyebamiji sodiq <oyebamijisodiq70@gmail.com>
2022-03-11 01:27:33 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Description: prints "Programming is like building a multilingual puzzle"
- *
- * Return: Always 0 (Success)
- */
+[3~[3~[D[D[D[D[D[D[D * main - main block
+ *Return: 0
+ */[A[D Return: 0[B[D[D[D[D[D[D[D[D[D
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-[A[A[A[A[A[A[A[A[A[A[A[A[B[B[/**[B[B[B[B[B[B[B[[[B[B[A}
