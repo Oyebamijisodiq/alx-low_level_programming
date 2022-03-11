1b7a7056d47e763ca9a91a3ab2eb2cb0baae79ae
oyebamiji sodiq <oyebamijisodiq70@gmail.com>
2022-03-11 09:57:48 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry point
- *
- * Description: prints "Programming is like building a multilingual puzzle"
- *
- * Return: Always 0 (Success)
+ * Description: prints "programming is like buildinv a puzzle"
+ * Return: (0) success
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
